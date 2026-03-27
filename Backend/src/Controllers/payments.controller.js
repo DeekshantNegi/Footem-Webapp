9abe746599152c23fa2c52d4c0ed7a22596caa08
Vivek Negi <viveknegi952@gmail.com>
2026-03-27 22:30:42 +0530

    feat: implement payment verification logic and enhance error handling in payment processing

--- Backend/src/Controllers/payments.controller.js
+++ Backend/src/Controllers/payments.controller.js
@@ -1,6 +1,10 @@
+import crypto from "crypto";
 import razorpay from "../Utils/razorpay";
 import asyncHandler from "../Utils/asyncHandler";
 import { Booking } from "../Models/booking.model";
+import { Payment } from "../Models/payment.model";
+import razorpay from "../Utils/razorpay.js";
+import ApiError from "../Utils/ApiError.js";
 
 const createOrder = asyncHandler(async (req, res)=>{
    
@@ -28,4 +32,48 @@ const createOrder = asyncHandler(async (req, res)=>{
   .json(new ApiResponse(200, { orderId: order.id, amount: order.amount }, "Order created successfully"));
 });
 
-export { createOrder };
+const verifyPayment = asyncHandler(async (req, res) => {
+   const { razorpay_order_id, razorpay_payment_id, razorpay_signature, bookingId } = req.body;
+   if(!razorpay_order_id || !razorpay_payment_id || !razorpay_signature || !bookingId){
+    throw new ApiError(400, "Missing information");
+   }
+   const body = razorpay_order_id + "|" + razorpay_payment_id;
+   const expectedSignature = crypto
+   .createHmac("sha256", process.env.RAZORPAY_KEY_SECRET)
+   .update(body)
+   .digest("hex");
+
+   if(expectedSignature !== razorpay_signature){
+    throw new ApiError(400, "Payment verification failed, invalid signature");
+}
+
+    const booking = await Booking.findById(bookingId);
+    if(!booking || booking.bookingStatus !== "pending"){
+        throw new ApiError(404, "Booking not found");
+    }   
+
+    const existingPayment = await Payment.findOne({razorpayPaymentId: razorpay_payment_id});
+    if(existingPayment){
+        throw new ApiError(400, "Payment already exists");
+   }
+
+   const payment = await Payment.create({
+    bookingId,
+    userId: booking.userId,
+    amount: booking.totalPrice,
+    razorpayOrderId: razorpay_order_id,
+    razorpayPaymentId: razorpay_payment_id,
+    razorpaySignature: razorpay_signature,
+    paymentStatus: "completed",
+   });
+
+   // Update booking status to confirmed
+   booking.bookingStatus = "confirmed";
+   booking.payment = payment._id;
+   await booking.save();
+
+    res.status(200)
+    .json(new ApiResponse(200, payment, "Payment verified and booking confirmed successfully"));
+});
+
+export { createOrder, verifyPayment };
